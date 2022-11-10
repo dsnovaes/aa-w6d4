f44f4357118c2eac0dc6f0d3ebc416689818e65c
Thomas Conger <t.e.conger@gmail.com>
2022-11-10 12:03:23 -0800

    added artwork share and some associations

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: artworks
+#
+#  id         :bigint           not null, primary key
+#  title      :string
+#  image_url  :string
+#  artist_id  :bigint
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Artwork < ApplicationRecord
     validates :title, :image_url, :artist_id, presence: true
     validates :title, uniqueness: { scope: :artist_id,
@@ -7,4 +18,9 @@ class Artwork < ApplicationRecord
         foreign_key: :artist_id,
         class_name: :User,
         dependent: :destroy
+
+    has_many :viewers,
+        foreign_key: :viewer_id,
+        class_name: :ArtworkShare
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,9 @@
 # Table name: users
 #
 #  id         :bigint           not null, primary key
-#  name       :string           not null
-#  email      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  username   :string           not null
 #
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
@@ -14,4 +13,12 @@ class User < ApplicationRecord
   has_many :artworks,
     foreign_key: :artist_id,
     class_name: :Artwork
+
+  has_many :views,
+    foreign_key: :viewer_id,
+    class_name: :ArtworkShare
+
+  has_many :shared_artworks,
+    through: :shares,
+    source: :
 end
