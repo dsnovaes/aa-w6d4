911b95e64eb9aa7da043fcc62c29fa8ef00296eb
Diego Novaes <dsnovaes1@gmail.com>
2022-11-10 13:53:00 -0800

    artwork_shares on the way

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -16,11 +16,15 @@ class Artwork < ApplicationRecord
 
     belongs_to :artist,
         foreign_key: :artist_id,
-        class_name: :User,
+        class_name: :User
+
+    has_many :artwork_shares, # former viewers
+        foreign_key: :viewer_id,
+        class_name: :ArtworkShare,
         dependent: :destroy
 
-    has_many :viewers,
-        foreign_key: :viewer_id,
-        class_name: :ArtworkShare
+    has_many :shared_viewers,
+        through: :artwork_shares,
+        source: :viewer
     
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,54 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-u1 = User.new({username:"alice"})
-u2 = User.new({username:"bob"})
-u3 = User.new({username:"charlie"})
-u4 = User.new({username:"dennis"})
-u5 = User.new({username:"edward"})
+ApplicationRecord.transaction do
+    puts 'Destroying tables...'
+    User.destroy_all
 
-u1.save!
-u2.save!
-u3.save!
-u4.save!
-u5.save!
+    puts 'Resetting id sequences...'
+    %w(users artworks artwork_shares).each do |table_name|
+        puts 'Resetting id sequences of ' + table_name.to_s
+        ApplicationRecord.connection.reset_pk_sequence!(table_name)
+    end
 
-a1 = Artwork.new(title: "Blue", image_url: "blue.jpg", artist_id: 1)
-a2 = Artwork.new(title: "Brown", image_url: "brown.jpg", artist_id: 1)
-a3 = Artwork.new(title: "Red", image_url: "red.jpg", artist_id: 2)
-a4 = Artwork.new(title: "Green", image_url: "green.jpg", artist_id: 3)
-a5 = Artwork.new(title: "Blue", image_url: "blu.jpg", artist_id: 4)
+    puts 'Creating seed data...'
+    u1 = User.new({username:"alice"})
+    u2 = User.new({username:"bob"})
+    u3 = User.new({username:"charlie"})
+    u4 = User.new({username:"dennis"})
+    u5 = User.new({username:"edward"})
+    u1.save!
+    u2.save!
+    u3.save!
+    u4.save!
+    u5.save!
 
-a1.save!
-a2.save!
-a3.save!
-a4.save!
-a5.save!
+    a1 = Artwork.new(title: "Blue", image_url: "blue.jpg", artist_id: 1)
+    a2 = Artwork.new(title: "Brown", image_url: "brown.jpg", artist_id: 1)
+    a3 = Artwork.new(title: "Red", image_url: "red.jpg", artist_id: 2)
+    a4 = Artwork.new(title: "Green", image_url: "green.jpg", artist_id: 3)
+    a5 = Artwork.new(title: "Blue", image_url: "blu.jpg", artist_id: 4)
+    a1.save!
+    a2.save!
+    a3.save!
+    a4.save!
+    a5.save!
+
+    as1 = ArtworkShare.new(artwork_id: 1, viewer_id: 1)
+    as2 = ArtworkShare.new(artwork_id: 2, viewer_id: 2)
+    as3 = ArtworkShare.new(artwork_id: 2, viewer_id: 1)
+    as4 = ArtworkShare.new(artwork_id: 3, viewer_id: 1)
+    as5 = ArtworkShare.new(artwork_id: 4, viewer_id: 1)
+    as6 = ArtworkShare.new(artwork_id: 5, viewer_id: 1)
+    as7 = ArtworkShare.new(artwork_id: 3, viewer_id: 2)
+    as1.save!
+    as2.save!
+    as3.save!
+    as4.save!
+    as5.save!
+    as6.save!
+    as7.save!
+
+    puts 'Done!'
+
+end
